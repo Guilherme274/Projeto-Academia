6e2a3f1ee4406b981145a20e973a871d26159041
Guilherme Paixão dos Santos <paixaoguilherme79@gmail.com>
2026-02-24 11:13:23 -0300

    :sparkles: feat: Substituindo propriedade Username por Email na LoginRequest

--- Back/TrackFocus.Application/DTOs/Request/LoginRequest.cs
+++ Back/TrackFocus.Application/DTOs/Request/LoginRequest.cs
@@ -2,5 +2,5 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TrackFocus.Application.DTOs.Request
 {
-    public record LoginRequest([Required] string Username,[Required] string Password, [Required] DateTime DataNascimento);
+    public record LoginRequest([Required] string Email,[Required] string Password);
 }
