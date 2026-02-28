0491e94da4409b31c3e4bbb0fff22f34c9c5a824
Guilherme Paixão dos Santos <paixaoguilherme79@gmail.com>
2026-02-28 19:42:02 -0300

    :recycle: refactor: Passando todas as propriedades de Usuário para LoginResponse

--- Back/TrackFocus.Application/DTOs/Security/Response/LoginResponse.cs
+++ Back/TrackFocus.Application/DTOs/Security/Response/LoginResponse.cs
@@ -1,4 +1,7 @@
 namespace TrackFocus.Application.DTOs.Request
 {
-    public record LoginResponse(int Id, string Email);
+    public record LoginResponse(string Id, string Email, string UserName, DateTime DataNascimento, string Token)
+    {
+        public LoginResponse() : this("", string.Empty, string.Empty, DateTime.MinValue, string.Empty) { }
+    }
 }
--- Back/TrackFocus.Application/DTOs/Security/Response/RegisterResponse.cs
+++
@@ -1,4 +0,0 @@
-namespace TrackFocus.Application.DTOs.Response
-{
-    public record RegisterResponse(string Id, string Email, string Username, DateTime DataNascimento);
-}
