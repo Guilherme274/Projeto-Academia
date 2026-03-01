e1995a7fa0f9b0ac26dad64753b372c9981a09bd
Guilherme Paixão dos Santos <paixaoguilherme79@gmail.com>
2026-03-01 05:12:52 -0300

    :sparkles: feat: Criando novas Migrations para atualizar Banco

--- Back/TrackFocus.Infraestructure/Data/Migrations/20260224041526_CriandoUsuarioDB.Designer.cs
+++ Back/TrackFocus.Infraestructure/Data/Migrations/20260224041526_CriandoUsuarioDB.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using TrackFocus.Infraestructure.Data;
+using TrackFocus.Infraestructure.Data.Security;
 
 #nullable disable
 
--- Back/TrackFocus.Infraestructure/Data/Migrations/SecurityContextModelSnapshot.cs
+++ Back/TrackFocus.Infraestructure/Data/Migrations/SecurityContextModelSnapshot.cs
@@ -4,7 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using TrackFocus.Infraestructure.Data;
+using TrackFocus.Infraestructure.Data.Security;
+
 
 #nullable disable
 
